0b59e24ba743148db4692389ba5256b3bda907d5
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-16 17:53:42 -0300

    ReactJS - Dica: como tratar campo numerico nao preenchido em formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 
+type FormData = {
+  salary?: number;
+}
 
 function App() {
 
+  const [formData, setFormData] = useState<FormData>({});
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name  = event.target.name;
+    setFormData({ ...formData, [name]: value});
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(formData.salary || Number.MAX_VALUE);
+  }
+
   return (
-    <h1>Olá mundo</h1>
+    <form onSubmit={handleSubmit}>
+      <input
+        name="salary"
+        value={formData.salary || ""} 
+        type="text" 
+        placeholder="Digite o número máximo"
+        onChange={handleInputChange}
+      />
+      <button type="submit">Enviar</button>
+    </form>
   )
 }
 
